3e4f4cca4d346c02e8230a8f1d900f6539ecc02d
lun-code <lun.code01@gmail.com>
2026-07-18 07:11:59 +0200

    Fix: Instant en timestamp de DTO en lugar de String.

--- str-producer/src/main/java/com/example/str_producer/dto/EventoDTO.java
+++ str-producer/src/main/java/com/example/str_producer/dto/EventoDTO.java
@@ -1,9 +1,11 @@
 package com.example.str_producer.dto;
 
+import java.time.Instant;
+
 public record EventoDTO(
 
         String userId,
         String eventType,
         String page,
-        String timestamp
+        Instant timestamp
 ) {}
